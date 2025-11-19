e13eaf240b55776fc456fe731213bba3a0c81743
kr0khmal <krokhmal436@gmail.com>
2025-11-20 00:21:42 +0100

    Create settings.py

---
+++ rental/core/settings.py
@@ -0,0 +1,15 @@
+import os
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST", "db"),
+        "PORT": os.getenv("DB_PORT", "3306"),
+        "OPTIONS": {
+            "charset": "utf8mb4",
+        },
+    }
+}
